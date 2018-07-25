0f4dfc90df37eebc6d0e9e879eb72d1b3932d51a
Sean Raven <sravendragon0@hotmail.com>
2018-07-24 21:53:54 -0700

    initial feature brainstorm

---
+++ Autograder.mm
@@ -0,0 +1,102 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1532488597656" ID="ID_335630043" MODIFIED="1532488607934" TEXT="Autograder">
+<node CREATED="1532488672732" ID="ID_1129978682" MODIFIED="1532493870284" POSITION="right" TEXT="Instructor Dashboard">
+<node CREATED="1532488714298" ID="ID_1769277855" MODIFIED="1532488722986" TEXT="Create Assignment">
+<node CREATED="1532488724580" ID="ID_864882972" MODIFIED="1532488730876" TEXT="Add Instructions"/>
+<node CREATED="1532493982416" ID="ID_1301374150" MODIFIED="1532493999071" TEXT="Enable/Disable Languages"/>
+<node CREATED="1532488731136" ID="ID_276955724" MODIFIED="1532488735878" TEXT="Add Ruberic"/>
+<node CREATED="1532488739327" ID="ID_1030145092" MODIFIED="1532488743279" TEXT="Add Public Tests"/>
+<node CREATED="1532488743902" ID="ID_677609685" MODIFIED="1532488747576" TEXT="Add Private Tests"/>
+</node>
+<node CREATED="1532494055105" ID="ID_1294039497" MODIFIED="1532494059652" TEXT="Grade Assignment">
+<node CREATED="1532494202545" ID="ID_965331495" MODIFIED="1532494222585" TEXT="Show submission alongside ruberic">
+<node CREATED="1532494253203" ID="ID_905783906" MODIFIED="1532494271146" TEXT="Checkboxes on individual ruberic items to give credit"/>
+</node>
+<node CREATED="1532494067358" ID="ID_885773212" MODIFIED="1532494088933" TEXT="Display Tests Passed/Failed"/>
+<node CREATED="1532494091077" ID="ID_280874042" MODIFIED="1532494136769" TEXT="Compare to other students&apos; code to flag plagiarism"/>
+<node CREATED="1532494146265" ID="ID_357996165" MODIFIED="1532494173707" TEXT="Add feedback comments to individual lines of code"/>
+</node>
+<node CREATED="1532488756183" ID="ID_1248498550" MODIFIED="1532488893606" TEXT="List Assignments and Statistics"/>
+<node CREATED="1532488894140" ID="ID_831999484" MODIFIED="1532488904972" TEXT="Display Grade Table"/>
+</node>
+<node CREATED="1532488700346" ID="ID_991303470" MODIFIED="1532493867367" POSITION="right" TEXT="Student Dashboard">
+<node CREATED="1532488914909" ID="ID_617350884" MODIFIED="1532488926332" TEXT="List Available / Completed Assignments"/>
+<node CREATED="1532488930393" ID="ID_985905842" MODIFIED="1532488933640" TEXT="Show Grades"/>
+<node CREATED="1532488933993" ID="ID_921563848" MODIFIED="1532488942303" TEXT="Show Instructor Feedback"/>
+</node>
+<node CREATED="1532488705090" ID="ID_1304804817" MODIFIED="1532492715539" POSITION="right" TEXT="Submission System">
+<node CREATED="1532488950783" ID="ID_475434842" MODIFIED="1532488956193" TEXT="Accept a Submission">
+<node CREATED="1532488957676" ID="ID_504644583" MODIFIED="1532493938468" TEXT="Code">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      file upload to submit source files or ZIP files
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1532488963953" ID="ID_467280088" MODIFIED="1532493973610" TEXT="Language">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Select language used (intructor determines supported languages)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1532488967559" ID="ID_1324444118" MODIFIED="1532488973470" TEXT="Compile/Run Code"/>
+<node CREATED="1532488974922" ID="ID_1655878296" MODIFIED="1532493815031" TEXT="Execute Public and Secret Tests">
+<node CREATED="1532493476528" ID="ID_65061495" MODIFIED="1532493483205" TEXT="Types of Tests">
+<node CREATED="1532493487355" ID="ID_955422016" MODIFIED="1532493491321" TEXT="Unit Tests">
+<node CREATED="1532493492476" ID="ID_767180161" MODIFIED="1532493495145" TEXT="BoostTest"/>
+<node CREATED="1532493495458" ID="ID_392602879" MODIFIED="1532493497574" TEXT="JUnit"/>
+<node CREATED="1532493498274" ID="ID_457158596" MODIFIED="1532493503284" TEXT="unittest.py"/>
+</node>
+<node CREATED="1532493512077" ID="ID_816210704" MODIFIED="1532493565722" TEXT="pexpect tests">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Write and execute a pexpect script to test interactive applications
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1532493516714" ID="ID_529075706" MODIFIED="1532493812556" TEXT="stdio tests">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      run program with given command-line arguments, input data piped to stdin, stdout captured and diff'd against expected output
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1532489012744" ID="ID_1941540090" MODIFIED="1532489019230" TEXT="Report on Grade">
+<node CREATED="1532493825192" ID="ID_596890007" MODIFIED="1532493838154" TEXT="Grading Options">
+<node CREATED="1532493839005" ID="ID_230782160" MODIFIED="1532493845289" TEXT="No credit if tests fail"/>
+<node CREATED="1532493847802" ID="ID_1846012430" MODIFIED="1532493855828" TEXT="Partial credit based on number of passed tests"/>
+<node CREATED="1532493856514" ID="ID_1290489114" MODIFIED="1532493862543" TEXT="Extra credit for certain tests"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ README.md
@@ -0,0 +1,4 @@
+# autograder
+A web app for grading programming homework
+
+Mind map created with [FreeMind](http://freemind.sourceforge.net/wiki/index.php/Main_Page)
